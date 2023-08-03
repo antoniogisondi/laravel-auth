8329d147b058b7762fff18c830d418dd9ca0a6b3
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-03 19:07:52 +0200

    Create db records

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +14,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=1; $i<10; $i++){
+            $project = new Project();
+            $project->titolo = $faker->word();
+            $project->descrizione = $faker->text();
+            $project->inizio_progetto = $faker->date();
+            $project->consegna_progetto = $faker->date();
+            $project->approvato = $faker->boolean();
+            $project->non_approvato = $faker->boolean();
+            $project->save();
+        }
     }
 }
