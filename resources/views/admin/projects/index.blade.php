ba01a18ebc85e6bda35ec62d568d9891e5116972
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-21 14:54:57 +0200

    Method show done

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -18,8 +18,6 @@
                                     <th>Titolo</th>
                                     <th>Descrizione progetto</th>
                                     <th>Data inizio</th>
-                                    <th>Data consegna</th>
-                                    <th>Feedback</th>
                                     <th>Strumenti</th>
                                 </tr>
                             </thead>
@@ -29,18 +27,8 @@
                                         <td>{{ $project->titolo}}</td>
                                         <td>{{ $project->descrizione}}</td>
                                         <td>{{ $project->inizio_progetto}}</td>
-                                        <td>{{ $project->consegna_progetto}}</td>
                                         <td>
-                                            @if ($project->approvato == 1 && $project->non_approvato == 0)
-                                                Questo progetto ha avuto un feedback positivo
-                                            @elseif($project->non_approvato == 1 && $project->approvato == 0)
-                                                Questo progetto ha avuto un feedback negativo
-                                            @else
-                                                Il progetto è scaduto
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <a href="#" class="btn btn-warning"><i class="fa-solid fa-eye"></i></a>
+                                            <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-warning"><i class="fa-solid fa-eye"></i></a>
                                             <a href="#" class="btn btn-success"><i class="fa-solid fa-pen-to-square"></i></a>
                                             <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
                                         </td>
