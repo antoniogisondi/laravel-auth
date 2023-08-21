e2aae93b71d5af4a9c0ba06a78fdbbd9ae7db8fe
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-21 21:31:53 +0200

    Add validation rules to form

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -24,7 +24,23 @@ class UpdateProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'titolo' => 'required|max:5',
+            'descrizione' => 'required|max:255',
+            'inizio_progetto' => 'required|integer',
+            'consegna_progetto' => 'required|integer'
+        ];
+    }
+
+    public function messages(){
+        return [
+            'titolo.required' => 'Il campo del titolo deve essere compilato',
+            'titolo.max' => 'Il campo del titolo deve contenere al massimo :max caratteri',
+            'descrizione.required' => 'Il campo della descrizione deve essere compilato',
+            'descrizione.max' => 'Il campo della descrizione deve contenere al massimo :max caratteri',
+            'inizio_progetto.required' => 'Il campo dell\' inizio del progetto deve essere compilato',
+            'inizio_progetto.integer' => 'Il campo dell\'inizio progetto deve essere di tipo numerico e deve contenere i seguenti caratteri: -',
+            'consegna_progetto.required' => 'Il campo dell\' inizio del progetto deve essere compilato',
+            'consegna_progetto.integer' => 'Il campo dell\'inizio progetto deve essere di tipo numerico e deve contenere i seguenti caratteri: -'
         ];
     }
 }
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -10,28 +10,38 @@
                     </div>
 
                     <div class="card-body">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                         <form action="{{ route('admin.projects.update', $project->id)}}" method="POST">
                             @csrf
                             @method('PUT')
                             <div class="mb-3">
                                 <label for="titolo" class="form-label">Inserisci il titolo</label>
-                                <input type="text" class="form-control" id="titolo" name="titolo" placeholder="Inserisci il titolo del progetto">
+                                <input type="text" class="form-control" id="titolo" name="titolo" placeholder="Inserisci il titolo del progetto" value="{{ old('titolo')}}">
                             </div>
                             <div class="mb-3">
                                 <label for="descrizione" class="form-label">Inserisci descrizione</label>
-                                <textarea class="form-control" id="descrizione" name="descrizione" rows="3" placeholder="Inserisci la descrizione del progetto">{{ old('$project->descrizione') }}</textarea>
+                                <textarea class="form-control" id="descrizione" name="descrizione" rows="3" placeholder="Inserisci la descrizione del progetto">{{ old('descrizione')}}</textarea>
                             </div>
                             <div class="mb-3">
                                 <label for="inizio_progetto" class="form-label">Inserisci la data di inizio</label>
-                                <input type="text" class="form-control" id="inizio_progetto" name="inizio_progetto">
+                                <input type="text" class="form-control" id="inizio_progetto" placeholder="Inserisci la data di inizio" name="inizio_progetto" value="{{ old('inizio_progetto')}}">
                             </div>
                             <div class="mb-3">
                                 <label for="consegna_progetto" class="form-label">Inserisci la data di consegna</label>
-                                <input type="text" class="form-control" id="consegna_progetto" name="consegna_progetto">
+                                <input type="text" class="form-control" id="consegna_progetto" placeholder="Inserisci la data di consegna" name="consegna_progetto" value="{{ old('consegna_progetto')}}">
                             </div>
 
                             <div class="submit mb-3">
                                 <button type="submit" class="btn btn-success">Modifica il tuo progetto</button>
+                                <a href="{{ route('admin.projects.index')}}" class="btn btn-primary">Torna ai progetti</a>
                             </div>
                         </form>
                     </div>
